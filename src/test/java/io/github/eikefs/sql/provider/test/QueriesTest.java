d30fb4af4a15fda6107aefea9394f7f181f55787
Lorenzo Guimarães de Oliveira <lorenzooguimaraes@gmail.com>
2020-07-05 01:36:08 -0300

    fix: get instance of Provider correctly

--- src/test/java/io/github/eikefs/sql/provider/test/QueriesTest.java
+++ src/test/java/io/github/eikefs/sql/provider/test/QueriesTest.java
@@ -11,7 +11,7 @@ import java.util.concurrent.ExecutionException;
 public class QueriesTest {
 
     public static void main(String[] args) throws ExecutionException, InterruptedException {
-        Database database = Provider.from().submit("memory");
+        Database database = Provider.getInstance().submit("memory");
 
         // Creating tables
         database.update(
